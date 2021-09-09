a17571fcb7390157c6c9ba5802eade6c29957835
Wag0nWheels <47494569+Wag0nWheels@users.noreply.github.com>
2021-09-09 16:46:44 +0300

    'попытка на обход ArgumentParser'

--- src/main.py
+++ src/main.py
@@ -1,21 +1,11 @@
 import os
-# from argparse import ArgumentParser
+
+
 os.environ['dist'] = '35'
 os.environ['server'] = '0'
 SERVER = bool(int(os.environ['server']))
 DISTANCE = int(os.environ['dist'])
-print(DISTANCE)
 
-# def parse_arguments():
-#     parser = ArgumentParser(__doc__)
-#     parser.add_argument("--server", "-s", help="run without pygame?", default=False)
-#     parser.add_argument("--dist", "-d", help="distance for grab", default=25)
-#     return parser.parse_args()
-#
-# args = parse_arguments()
-# print(args)
-# SERVER = args.server
-# DISTANCE = args.dist
 
 
 def main(test_class):
